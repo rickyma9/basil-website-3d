b5cd9b1d0727b6dbb6f779cf02bced4d011a3b60
rickyma9 <48923521+rickyma9@users.noreply.github.com>
2020-05-22 18:31:53 -0400

    rearranged some files and styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import '../styles/App.css';
+import Table from './Table'
+import Header from './Header'
+import About from './About'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div id="about">
+        <h2>About me: </h2>
+      <About />
+      </div>
+      <div id="projects">
+      <h2>Projects I've worked on:</h2>
+      <Table headings={["Class / Event", "Project Name", "Project Description"]} body="Projects" />
+      </div>
+      <div id="courses">
+      <h2>Courses I've taken:</h2>
+      <Table headings={["Course Code", "Course Title", "Semester Taken", "Course Description"]} body="Courses" />
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,50 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import Table from './components/Table'
-import Header from './components/Header'
-import About from './components/About'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    {/* <div id="projects">
-      <h3>Projects I've worked on:</h3>
-      <Table headings={["Class / Event", "Project Name", "Project Description"]} body="Projects" />
-    </div>
-    <br />
-    <div id="courses">
-      <h3>Courses I've taken:</h3>
-      <Table headings={["Course Code", "Course Title", "Semester Taken", "Course Description"]} body="Courses" />
-    </div> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h2>About me: </h2>
-    <About />
-  </React.StrictMode>,
-  document.getElementById('about')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//   </React.StrictMode>,
+//   document.getElementById('about')
+// );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h2>Projects I've worked on:</h2>
-    <Table headings={["Class / Event", "Project Name", "Project Description"]} body="Projects" />
-  </React.StrictMode>,
-  document.getElementById('projects')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+    
+//   </React.StrictMode>,
+//   document.getElementById('projects')
+// );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h2>Courses I've taken:</h2>
-    <Table headings={["Course Code", "Course Title", "Semester Taken", "Course Description"]} body="Courses" />
-  </React.StrictMode>,
-  document.getElementById('courses')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+    
+//   </React.StrictMode>,
+//   document.getElementById('courses')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
